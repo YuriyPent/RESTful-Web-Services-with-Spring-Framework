408a26c34a2219969ff93061ddf21f7fc96328df
Pentyugov Yuriy <Seaman_on_watch@mail.ru>
2019-06-14 09:54:46 +0300

    Update MobileAppWsApplication.java

--- src/main/java/ru/appsdeveloperblock/app/ws/MobileAppWsApplication.java
+++ src/main/java/ru/appsdeveloperblock/app/ws/MobileAppWsApplication.java
@@ -5,10 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MobileAppWsApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MobileAppWsApplication.class, args);
 	}
-
 }
-
